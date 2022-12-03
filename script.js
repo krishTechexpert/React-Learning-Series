dc38de048450fadb795cde8ca6c4018d60b0619e
krishTechexpert <68417802+krishTechexpert@users.noreply.github.com>
2022-12-03 17:58:58 +0530

    custom props type checking behind logic

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 const root = document.getElementById('root');
 
-function tick() {
-  const time = new Date().toLocaleTimeString();
-  const element = (
+//const element = React.createElement('div',{className:'text',children:'hello kk'});
+export const Person = ({ firstName, lastName = 'kumar' }) => {
+  return (
     <div>
-      <div>hello world</div>
-      <input type="text" value={time} />
-      <br></br>
-      <input type="text" value={time} />
-      <span>${time}</span>
+      hello {firstName} {lastName}
     </div>
   );
-  ReactDOM.render(element, root);
-  //root.innerHTML = element;
-}
+};
 
-tick();
+// const PropTypes = {
+//   string(props, propNames, componentName) {
+//     if (typeof props[propNames] !== 'string') {
+//       return new Error(
+//         `Hey Component ${componentName} has ${propNames} which accept string value but you are passing  value ${props[propNames]} `
+//       );
+//     }
+//   },
+// };
 
-setInterval(tick, 1000);
+Person.propTypes = {
+  firstName: PropTypes.string.isRequired,
+  lastName: PropTypes.string.isRequired,
+};
+
+console.log(Person.propTypes);
+
+const element = <Person firstName={false} />;
+
+ReactDOM.render(element, root);
