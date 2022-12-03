9a42b2b429dbcf8d8fdcd703313f25f7ff7ceee5
krishTechexpert <68417802+krishTechexpert@users.noreply.github.com>
2022-12-03 19:37:56 +0530

    React Style for components

--- script.js
+++ script.js
@@ -1,31 +1,15 @@
 const root = document.getElementById('root');
 
-//const element = React.createElement('div',{className:'text',children:'hello kk'});
-export const Person = ({ firstName, lastName = 'kumar' }) => {
-  return (
-    <div>
-      hello {firstName} {lastName}
-    </div>
-  );
-};
+function Box({className,children,style}){
+  return <div className={`box ${className}`}  style={{...style,fontStyle:'italic'}} >{children} lightbox</div>
+}
 
-// const PropTypes = {
-//   string(props, propNames, componentName) {
-//     if (typeof props[propNames] !== 'string') {
-//       return new Error(
-//         `Hey Component ${componentName} has ${propNames} which accept string value but you are passing  value ${props[propNames]} `
-//       );
-//     }
-//   },
-// };
-
-Person.propTypes = {
-  firstName: PropTypes.string.isRequired,
-  lastName: PropTypes.string.isRequired,
-};
-
-console.log(Person.propTypes);
-
-const element = <Person firstName={false} />;
+const element = (
+  <>
+<Box  className="box--small" children="small" style={{backgroundColor:'pink'}}/>
+<Box  className="box--md" children="medium" style={{backgroundColor:'blue',color:'#fff'}}/>
+<Box  className="box--lg"  style={{backgroundColor:'#000',color:'#fff'}}>Large</Box>
+</>
+);
 
 ReactDOM.render(element, root);
