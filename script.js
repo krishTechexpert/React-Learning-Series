1747c371e54566b0de6e0193ade1e29c32411cb8
krishTechexpert <68417802+krishTechexpert@users.noreply.github.com>
2022-12-06 18:27:18 +0530

    React custom hooks with localstorage

--- script.js
+++ script.js
@@ -6,32 +6,29 @@ const initialValue = {
   isChecked: false,
 };
 
-// way-1: good Approach
 
-function App() {
+const useLocalStorage =(key,defaultValue='') => {
   const [person,setPerson] =React.useState(
-    //JSON.parse(window.localStorage.getItem('PERSON')) || '' // this will rendered each time to check localstorage object when state updated
-
-    // solution is to used lazy initializer ( that is used arrow function) inside useState hooks which runs only first time
 
     () => {
-      console.log('hello')
-      return JSON.parse(window.localStorage.getItem('PERSON')) || ''
+      return JSON.parse(localStorage.getItem(key)) || defaultValue
     }
-
- //save lots of code like that see way-2 example below
- // // place first to fetch value from local storage
-//   React.useEffect(() => {
-//     const items = JSON.parse(localStorage.getItem('PERSON'))
-//   console.log("final",items)
-//   if(items){
-//     setPerson(items)
-//   }
-//   },[])
-
   )
 
-console.log('rendered')
+  
+React.useEffect(() =>{
+  console.log('useEffect rendered...')
+  localStorage.setItem(key,JSON.stringify(person))
+},[key,person]) 
+
+
+  return [person,setPerson]
+
+}
+
+function App() {
+  const [person,setPerson] = useLocalStorage('PERSON')
+
   const inputHandler = (e) => {
      let myName= e.target.name;
      let val = e.target.value;
@@ -51,11 +48,6 @@ const checkInputHandler = (e) => {
 
 
 
-React.useEffect(() =>{
-  console.log('useEffect rendered...')
-  window.localStorage.setItem('PERSON',JSON.stringify(person))
-},[person]) // when any value of person updated this useEffect runs
-
   const {firstName,lastName,isChecked} = person;
 
   return (
@@ -71,94 +63,5 @@ React.useEffect(() =>{
     </>
   );
 }
-
-// way-2
-
-// function App() {
-//   const [person, setPerson] = React.useState(initialValue);
-
-//   const inputHandler = (e) => {
-//     let myName = e.target.name;
-//     let val = e.target.value;
-
-//     setPerson((prevState) => ({
-//       ...prevState,
-//       [myName]: val,
-//     }));
-//   };
-
-//   const checkInputHandler = (e) => {
-//     setPerson((prevState) => ({
-//       ...prevState,
-//       isChecked: e.target.checked,
-//     }));
-//   };
-
-// // place first to fetch value from local storage
-//   React.useEffect(() => {
-//     const items = JSON.parse(localStorage.getItem('PERSON'))
-//   console.log("final",items)
-//   if(items){
-//     setPerson(items)
-//   }
-//   },[])
-
-//   React.useEffect(() => {
-//     console.log('useEffect rendered...');
-//     localStorage.setItem('PERSON', JSON.stringify(person));
-//   }, [person]);
-
- 
-
-//   const { firstName, lastName, isChecked } = person;
-
-//   return (
-//     <>
-//       <h1>manage State using useState hooks</h1>
-//       <p>
-//         hello 
-//         {firstName || lastName ? (
-//           <strong>
-//             {firstName} 
-//             {lastName}
-//           </strong>
-//         ) : (
-//           'please type here'
-//         )}
-//       </p>
-//       <form>
-//         <label>
-//           FirstName:{' '}
-//           <input
-//             value={firstName}
-//             name="firstName"
-//             onChange={(e) => inputHandler(e)}
-//             type="text"
-//           />
-//         </label>
-//         <br />
-//         <label>
-//           LastName:{' '}
-//           <input
-//             value={lastName}
-//             name="lastName"
-//             onChange={(e) => inputHandler(e)}
-//             type="text"
-//           />
-//         </label>
-//         <br />
-//         <label>
-//           <input
-//             type="checkbox"
-//             checked={isChecked}
-//             onChange={(e) => checkInputHandler(e)}
-//           />{' '}
-//           remember me{' '}
-//         </label>
-//         <br />
-//       </form>
-//     </>
-//   );
-// }
 
 ReactDOM.render(<App />, root);
