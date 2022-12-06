d551b7d504562dcbf83925b2a8b9f8551d31c919
krishTechexpert <68417802+krishTechexpert@users.noreply.github.com>
2022-12-06 19:11:59 +0530

    counter custom hooks

--- script.js
+++ script.js
@@ -1,65 +1,33 @@
 const root = document.getElementById('root');
 
-const initialValue = {
-  firstName: '',
-  lastName: '',
-  isChecked: false,
+const useCounter = (defaultValue = 10) => {
+  const [count, setCount] = React.useState(0);
+
+  const incrementHandler = () => {
+    console.log('Inc render...');
+    setCount((prevCount) => prevCount + defaultValue);
+  };
+
+  const decrementHandler = () => {
+    console.log('Dec render...');
+    if (count > 0) {
+      setCount((prevCount) => prevCount - defaultValue);
+    } else {
+      setCount(count);
+    }
+  };
+
+  return [count, incrementHandler, decrementHandler];
 };
 
-
-const useLocalStorage =(key,defaultValue='') => {
-  const [person,setPerson] =React.useState(
-
-    () => {
-      return JSON.parse(localStorage.getItem(key)) || defaultValue
-    }
-  )
-
-  
-React.useEffect(() =>{
-  console.log('useEffect rendered...')
-  localStorage.setItem(key,JSON.stringify(person))
-},[key,person]) 
-
-
-  return [person,setPerson]
-
-}
-
 function App() {
-  const [person,setPerson] = useLocalStorage('PERSON')
-
-  const inputHandler = (e) => {
-     let myName= e.target.name;
-     let val = e.target.value;
-
-      setPerson(prevState => ({
-        ...prevState,
-        [myName]:val
-      }))
-  }
-
-const checkInputHandler = (e) => {
-  setPerson(prevState => ({
-    ...prevState,
-    isChecked:e.target.checked
-  }))
-}
-
-
-
-  const {firstName,lastName,isChecked} = person;
+  const [count, incrementHandler, decrementHandler] = useCounter(5);
 
   return (
-        <>
-
-      <h1>manage State using useState hooks</h1>
-      <p>hello, {firstName || lastName?<strong>{firstName} {lastName}</strong>:'please type here'}</p>
-      <form>
-       <label>FirstName: <input value={firstName} name="firstName" onChange={(e) =>inputHandler(e)}  type="text" /></label><br/>
-       <label>LastName: <input value={lastName} name="lastName" onChange={(e) =>inputHandler(e)} type="text" /></label><br />
-       <label><input type="checkbox" checked={isChecked}  onChange={(e) =>checkInputHandler(e)}   /> remember me </label><br />
-       </form>
+    <>
+      <button onClick={decrementHandler}>-</button>
+      <span>Counter - {count}</span>
+      <button onClick={incrementHandler}>+ </button>
     </>
   );
 }
