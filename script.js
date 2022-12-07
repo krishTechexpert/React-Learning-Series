081296bb966b13adbeb6412cb982af83b8f70260
krishTechexpert <68417802+krishTechexpert@users.noreply.github.com>
2022-12-07 19:08:35 +0530

    react hook flow

--- script.js
+++ script.js
@@ -1,34 +1,115 @@
 const root = document.getElementById('root');
 
-const useCounter = (defaultValue = 10) => {
-  const [count, setCount] = React.useState(0);
+function Child() {
+  console.log('%c    Child: render start', 'color: MediumSpringGreen')
 
-  const incrementHandler = () => {
-    console.log('Inc render...');
-    setCount((prevCount) => prevCount + defaultValue);
-  };
+  const [count, setCount] = React.useState(() => {
+    console.log('%c    Child: useState callback', 'color: tomato')
+    return 0
+  })
 
-  const decrementHandler = () => {
-    console.log('Dec render...');
-    if (count > 0) {
-      setCount((prevCount) => prevCount - defaultValue);
-    } else {
-      setCount(count);
+  React.useEffect(() => {
+    console.log('%c    Child: useEffect no deps', 'color: LightCoral')
+    return () => {
+      console.log(
+        '%c    Child: useEffect no deps cleanup',
+        'color: LightCoral',
+      )
     }
-  };
+  })
 
-  return [count, incrementHandler, decrementHandler];
-};
+  React.useEffect(() => {
+    console.log(
+      '%c    Child: useEffect empty deps',
+      'color: MediumTurquoise',
+    )
+    return () => {
+      console.log(
+        '%c    Child: useEffect empty deps cleanup',
+        'color: MediumTurquoise',
+      )
+    }
+  }, [])
+
+  React.useEffect(() => {
+    console.log('%c    Child: useEffect with dep', 'color: HotPink')
+    return () => {
+      console.log(
+        '%c    Child: useEffect with dep cleanup',
+        'color: HotPink',
+      )
+    }
+  }, [count])
+
+  const element = (
+    <button onClick={() => setCount(previousCount => previousCount + 1)}>
+      {count}
+    </button>
+  )
+
+  console.log('%c    Child: render end', 'color: MediumSpringGreen')
+
+  return element
+}
+
+
 
 function App() {
-  const [count, incrementHandler, decrementHandler] = useCounter(5);
+console.log('%c App: render start', `color:MediumSpringGreen`)
+const [showChild,setShowChild] = React.useState(() => {
+  console.log('%cApp: useState callback', 'color: tomato')
+  return false
+})
+
+
+React.useEffect(() => {
+  console.log('%cApp: useEffect no deps', 'color: LightCoral')
+  return () => {
+    console.log('%cApp: useEffect no deps cleanup', 'color: LightCoral')
+  }
+})
+
+React.useEffect(() => {
+  console.log('%cApp: useEffect empty deps', 'color: MediumTurquoise')
+  return () => {
+    console.log(
+      '%cApp: useEffect empty deps cleanup',
+      'color: MediumTurquoise',
+    )
+  }
+}, [])
+
+React.useEffect(() => {
+  console.log('%cApp: useEffect with dep', 'color: HotPink')
+  return () => {
+    console.log('%cApp: useEffect with dep cleanup', 'color: HotPink')
+  }
+}, [showChild])
+
+console.log('%cApp: render end', 'color: MediumSpringGreen')
 
   return (
     <>
-      <button onClick={decrementHandler}>-</button>
-      <span>Counter - {count}</span>
-      <button onClick={incrementHandler}>+ </button>
-    </>
+          <label>
+            <input
+              type="checkbox"
+              checked={showChild}
+              onChange={e => setShowChild(e.target.checked)}
+            />{' '}
+            show child
+          </label>
+          <div
+            style={{
+              padding: 10,
+              margin: 10,
+              height: 60,
+              width: 90,
+              border: 'solid',
+            }}
+          >
+            {showChild ? <Child /> : null}
+          </div>
+        </>
   );
 }
 
